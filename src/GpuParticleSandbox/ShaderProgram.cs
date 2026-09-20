dde3d830240af2f685569f045f88bed208d1a414
Your Name <you@example.com>
2026-09-20 14:03:52 +0000

    docs: add XML documentation to ShaderProgram public members

--- src/GpuParticleSandbox/ShaderProgram.cs
+++ src/GpuParticleSandbox/ShaderProgram.cs
@@ -7,13 +7,30 @@ namespace GpuParticleSandbox;
 /// Thin wrapper around a linked GL program plus a uniform-location cache.
 /// Handles both the render pair (vert+frag) and standalone compute programs.
 /// </summary>
+/// <remarks>
+/// All operations require a current OpenGL context. Failing to set a context before calling
+/// any method will result in undefined behavior or exceptions.
+/// </remarks>
 public sealed class ShaderProgram : IDisposable
 {
+    /// <summary>
+    /// Gets the underlying OpenGL program handle.
+    /// </summary>
     public int Handle { get; }
     private readonly Dictionary<string, int> _uniformCache = new();
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ShaderProgram"/> class.
+    /// </summary>
+    /// <param name="handle">The OpenGL program handle.</param>
     private ShaderProgram(int handle) => Handle = handle;
 
+    /// <summary>
+    /// Creates and loads a compute shader program from the specified file path.
+    /// </summary>
+    /// <param name="path">The path to the compute shader file.</param>
+    /// <returns>A linked <see cref="ShaderProgram"/> instance.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the shader fails to compile or the program fails to link.</exception>
     public static ShaderProgram FromCompute(string path)
     {
         ArgumentException.ThrowIfNullOrEmpty(path);
@@ -22,6 +39,13 @@ public sealed class ShaderProgram : IDisposable
         return new ShaderProgram(program);
     }
 
+    /// <summary>
+    /// Creates and loads a vertex and fragment shader program from the specified file paths.
+    /// </summary>
+    /// <param name="vertPath">The path to the vertex shader file.</param>
+    /// <param name="fragPath">The path to the fragment shader file.</param>
+    /// <returns>A linked <see cref="ShaderProgram"/> instance.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the shaders fail to compile or the program fails to link.</exception>
     public static ShaderProgram FromVertexFragment(string vertPath, string fragPath)
     {
         ArgumentException.ThrowIfNullOrEmpty(vertPath);
@@ -32,23 +56,49 @@ public sealed class ShaderProgram : IDisposable
         return new ShaderProgram(program);
     }
 
+    /// <summary>
+    /// Binds this program as the current rendering program.
+    /// </summary>
     public void Use() => GL.UseProgram(Handle);
 
+    /// <summary>
+    /// Sets a float uniform value.
+    /// </summary>
+    /// <param name="name">The name of the uniform variable.</param>
+    /// <param name="value">The value to set.</param>
     public void SetFloat(string name, float value)
     {
         ArgumentException.ThrowIfNullOrEmpty(name);
         GL.Uniform1(Location(name), value);
     }
+
+    /// <summary>
+    /// Sets an unsigned integer uniform value.
+    /// </summary>
+    /// <param name="name">The name of the uniform variable.</param>
+    /// <param name="value">The value to set.</param>
     public void SetUInt(string name, uint value)
     {
         ArgumentException.ThrowIfNullOrEmpty(name);
         GL.Uniform1(Location(name), value);
     }
+
+    /// <summary>
+    /// Sets a 2D vector uniform value.
+    /// </summary>
+    /// <param name="name">The name of the uniform variable.</param>
+    /// <param name="value">The value to set.</param>
     public void SetVector2(string name, Vector2 value)
     {
         ArgumentException.ThrowIfNullOrEmpty(name);
         GL.Uniform2(Location(name), value);
     }
+
+    /// <summary>
+    /// Sets an integer uniform value.
+    /// </summary>
+    /// <param name="name">The name of the uniform variable.</param>
+    /// <param name="value">The value to set.</param>
     public void SetInt(string name, int value)
     {
         ArgumentException.ThrowIfNullOrEmpty(name);
@@ -116,5 +166,8 @@ public sealed class ShaderProgram : IDisposable
         return program;
     }
 
+    /// <summary>
+    /// Releases all resources used by the <see cref="ShaderProgram"/>.
+    /// </summary>
     public void Dispose() => GL.DeleteProgram(Handle);
 }
